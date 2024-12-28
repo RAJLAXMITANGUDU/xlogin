2cdbc532509f57f66f0bb80039aa8d840074646b
RAJLAXMI TANGUDU <rajlaxmitangudu@gmail.com>
2024-12-28 21:01:59 +0530

    Test commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React,{useState} from "react";
+const App=()=>{
+  const [username,setUsername]=useState("");
+  const [password,setPassword]=useState("");
+  const [message,setMessage]=useState("");
+  // const [error,setError]=useState("");
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(username || !password){
+      setMessage("Both fields are mandatory");
+      return;
+    }
+    if(username==="user" && password==="password"){
+      setMessage("Welcome,user!");
+    } else {
+      setMessage("Invalid username or password");
+    }
+  };
+  return(
+
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+        <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+export default App
